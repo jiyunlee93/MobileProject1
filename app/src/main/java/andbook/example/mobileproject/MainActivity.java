9065a4277f2f795123c3326c9b3060c57a0f4162
jiyunLee <ingjiyun@hanmail.net>
2018-12-12 21:15:13 +0900

    webCrawling module

--- app/src/main/java/andbook/example/mobileproject/MainActivity.java
+++ app/src/main/java/andbook/example/mobileproject/MainActivity.java
@@ -3,8 +3,10 @@ package andbook.example.mobileproject;
 import android.app.FragmentManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -17,6 +19,11 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -28,7 +35,10 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         System.loadLibrary("native-lib");
     }
     GoogleMap mMap;
-
+    private String htmlPageUrl = "https://search.naver.com/search.naver?sm=top_hty&fbm=0&ie=utf8&query=서울특별시+중구+의주로2가+어린이집"; //파싱할 홈페이지의 URL주소
+    private TextView textviewHtmlDocument;
+    private String htmlContentInStringFormat="";
+    int cnt=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +57,8 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         CameraPosition carPos=mMap.getCameraPosition();
         System.out.println("latitude:::::::::::::::::::::::::::::"+ carPos.target.latitude);
         System.out.println("longitude:"+ carPos.target.longitude);
+        textviewHtmlDocument = (TextView)findViewById(R.id.text_crawling);
+        textviewHtmlDocument.setMovementMethod(new ScrollingMovementMethod()); //스크롤 가능한 텍스트뷰로 만들기
 
         final Geocoder geocoder = new Geocoder(this);
         // 위도,경도 입력 후 변환 버튼 클릭
@@ -119,6 +131,76 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         //     System.out.println("주소 변환 실패");
         // }
 
+    }
+
+    private class JsoupAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                Document doc = Jsoup.connect(htmlPageUrl).get();
+
+
+                ////테스트1
+                //Elements titles= doc.select("div.news-con h1.tit-news");
+//
+                //System.out.println("-------------------------------------------------------------");
+                //for(Element e: titles){
+                //    System.out.println("title: " + e.text());
+                //    htmlContentInStringFormat += e.text().trim() + "\n";
+                //}
+//
+                ////테스트2
+                //titles= doc.select("div.news-con h2.tit-news");
+//
+                //System.out.println("-------------------------------------------------------------");
+                //for(Element e: titles){
+                //    System.out.println("title: " + e.text());
+                //    htmlContentInStringFormat += e.text().trim() + "\n";
+                //}
+//
+                //테스트3
+                Elements titles= doc.select("ul.lst_map dl.info_area");
+
+                System.out.println("-------------------------------------------------------------");
+                for(Element e: titles){
+                    String a = e.text();
+                    String name = a.split("보내기")[0];
+                    if(name.contains("...")){
+                        System.out.println("name= "+ name);
+                        int index = name.lastIndexOf("어");
+                        System.out.println("index= "+ index);
+                        name=name.substring(0,index-1);
+                        System.out.println("AFtername= "+ name);
+                        name=name+"어린이집";
+                    }
+                    System.out.println("title: " + e.text());
+                    System.out.println("이름 :"+name);
+                    System.out.println("주소 :"+a.split("열기")[1]);
+                    System.out.println("/////////////////////////////////////////");
+                    htmlContentInStringFormat += e.text().trim() + "\n";
+                }
+                System.out.println("-------------------------------------------------------------");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            textviewHtmlDocument.setText(htmlContentInStringFormat);
+        }
+    }
+    public void crawling (View v){
+        System.out.println( (cnt+1) +"번째 파싱");
+        JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+        jsoupAsyncTask.execute();
     }
 
 }
